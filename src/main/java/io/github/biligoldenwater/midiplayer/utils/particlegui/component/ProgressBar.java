85da2209f9932a3006d75869635abf4db18d955f
BiliGoldenWater <2439577268@qq.com>
2022-01-16 06:23:00 +0800

    添加 ProgressBar

---
+++ src/main/java/io/github/biligoldenwater/midiplayer/utils/particlegui/component/ProgressBar.java
@@ -0,0 +1,73 @@
+package io.github.biligoldenwater.midiplayer.utils.particlegui.component;
+
+import io.github.biligoldenwater.midiplayer.utils.particlegui.PixelColor;
+import io.github.biligoldenwater.midiplayer.utils.particlegui.interfaces.Component;
+import org.bukkit.Color;
+
+public class ProgressBar extends Component {
+    private boolean isHorizontal;
+    private double percent;
+    private PixelColor color;
+
+    public ProgressBar(int x, int y, int width, int height, boolean isHorizontal) {
+        super(x, y, width, height, false);
+
+        this.color = new PixelColor(Color.BLUE);
+        this.isHorizontal = isHorizontal;
+
+        render();
+    }
+
+    public ProgressBar(int x, int y, int width, int height) {
+        this(x, y, width, height, true);
+    }
+
+    @Override
+    public void render() {
+        if (!isHorizontal) {
+            for (int i = 0; i < pixels.length * percent * 0.01; i++) {
+                pixels[i] = color;
+            }
+        } else {
+            long pixelNum = 1;
+
+            int width = getWidth();
+            int height = getHeight();
+
+            for (int x = 0; x < width; x++) {
+                for (int y = height - 1; y >= 0; y--) {
+                    if (pixelNum > pixels.length * percent * 0.01) break;
+                    pixels[x + y * width] = color;
+                    pixelNum++;
+                }
+            }
+        }
+    }
+
+    public double getPercent() {
+        return percent;
+    }
+
+    public void setPercent(double percent) {
+        onChange();
+        this.percent = Math.min(percent, 100f);
+    }
+
+    public PixelColor getColor() {
+        return color;
+    }
+
+    public void setColor(PixelColor color) {
+        onChange();
+        this.color = color;
+    }
+
+    public boolean isHorizontal() {
+        return isHorizontal;
+    }
+
+    public void setHorizontal(boolean horizontal) {
+        onChange();
+        isHorizontal = horizontal;
+    }
+}
